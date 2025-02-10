06f5c1a17a10e1034e4990415e6d1be3044b4018
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-02-10 15:40:46 +0100

    lib/default: remove mkLuaExprs

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,8 @@
-let
-  inherit (builtins) map;
-in {
+{
   kkts = {
     mkLuaExpr = expr: {
       inherit expr;
       _type = "lua-inline";
     };
-    mkLuaExprs = exprs: (map (expr: {
-        inherit expr;
-        _type = "lua-inline";
-      })
-      exprs);
   };
 }
