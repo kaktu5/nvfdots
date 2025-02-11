bd389da83d3f6b8a84e3707ddaf48031bb001b6d
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-02-11 16:18:54 +0100

    modules/remaps: fix remaps

--- modules/remaps.nix
+++ modules/remaps.nix
@@ -7,9 +7,8 @@ _: {
       "y".action = "\"+y";
       "Y".action = "\"+Y";
       "<leader>r" = {action = ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>";} // {silent = false;};
-      "<leader>x".action = "<cmd>!chmod +x %<CR>";
-      "<leader>szz".action = "<cmd>w !sudo tree %<CR>";
-      "<leader>cd".action = "<cmd>cd %:p:h<CR>";
+      "<leader>x".action = ":!chmod +x %<CR>";
+      "<leader>cd".action = ":cd %:p:h<CR>";
     };
     visual = {
       "J".action = ":m '>+1<CR>gv=gv";
